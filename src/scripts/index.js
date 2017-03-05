a3c73f4f5e34cab20ac79185e7c63485a4e70cd1
Mark Williams <markwill@hotmail.co.uk>
2017-03-05 14:01:49 +0000

    Feature/css preprocess (#1)
    * Added css processing/bundling via webpack
    
    * Added css processing/bundling via webpack
    
    * Added css preprocessing - currently generates processed file is ;dist' - sort this!
    
    * Added sass (scss) pre-processing
    
    * Pre-processing (of a very basic!!) scss file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,11 @@
 import $ from './select';
+import '../style/style.scss';
 
 window.onload = function onLoad() {
   const ps = $('p');
-  for (let i=0; i < ps.length; i++) {
-    ps[i].style.color = '#444488';
+  for (let i = 0; i < ps.length; i++) {
+    ps[i].className = 'enhanced';
   }
 };
-
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
   entry: './src/scripts/index.js',
@@ -6,6 +12,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  watch: true,
   module: {
     loaders: [
       {
@@ -16,6 +23,25 @@ module.exports = {
           presets: ['es2015']
         }
       }
+    ],
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader' // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }
+        ]
+      }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+  ]
 };
